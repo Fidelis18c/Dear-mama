52598e42acc4cc0c0f05688a00ef5299991872af
fidelis18 <kilulu12@gmail.com>
2026-01-16 00:22:12 +0300

    ContactPage changes

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -5,61 +5,63 @@ import contactImage from '../assets/Contact1.png'
 const ContactUs = () => {
   return (
     <> 
-   
+    
       <div className=''>
         <img src={contactImage} 
         alt="Contact Us" 
         className='w-full h-[40rem]' />
       </div>
-     
-      <div className="w-full max-w-4xl px-6">
-   
-    <h2 className="text-2xl font-semibold mb-10 text-gray-800">
-      Write to us</h2>
 
-   <div className='flex items-center gap-4'>
-     <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mb-10">
+<div className='flex items-center'>
+   
+      <div className="w-full px-6 bg-customPink">
+        <h2 className="text-2xl font-semibold mb-10 text-gray-800">Write to us</h2>
+      <div className='flex items-center gap-4'>
+      <div>
+       <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mb-10">
       
-      <div>
-        <label className="block text-sm text-gray-600 mb-2">Name</label>
-        <input
-          type="text"
+        <div>
+         <label className="block text-sm text-gray-600 mb-2">Name</label>
+          <input
+           type="text"
           className="w-full bg-transparent border-b border-gray-500 focus:outline-none focus:border-black"
-        />
-      </div>
+         />
+         </div>
 
-      <div>
-        <label className="block text-sm text-gray-600 mb-2">Email</label>
-        <input
-          type="email"
-          className="w-full bg-transparent border-b border-gray-500 focus:outline-none focus:border-black"
-        />
-      </div>
+       <div>
+         <label className="block text-sm text-gray-600 mb-2">Email</label>
+          <input
+           type="email"
+           className="w-full bg-transparent border-b border-gray-500 focus:outline-none focus:border-black"
+         />
+       </div>
 
-    </div>
-
+     
+     </div>
    
-    <div className="mb-12">
-      <label className="block text-sm text-gray-600 mb-4">
-        Write something...
-      </label>
-      <textarea
-        rows="3"
-        className="w-full bg-transparent border-b border-gray-500 focus:outline-none focus:border-black resize-none"
-      ></textarea>
-    </div>
+
+
+     <div className="mb-12">
+       <label className="block text-sm text-gray-600 mb-4">
+         Write something...
+       </label>
+       <textarea
+         rows="3"
+         className="w-full bg-transparent border-b border-gray-500 focus:outline-none focus:border-black resize-none"
+       ></textarea>
+     </div>
 
 
     <button
-      className="bg-orange-500 text-white px-6 py-3 text-sm tracking-wide hover:bg-orange-600 transition"
-    >
+      className="bg-orange-500 text-white px-6 py-3 text-sm tracking-wide hover:bg-orange-600 transition">
       SEND A MESSAGE
     </button>
-    </div>
   </div>
+   </div>
+ </div>
 
-     <div>
+
+     <div className='bg-white rounded-lg text-black h-19 pl-10'>
         <h1>CONTACT</h1>
 
         <h2>Address</h2>
@@ -71,9 +73,9 @@ const ContactUs = () => {
 
         <h2>Phone Number</h2>
         <p>+255694007665</p>
+     </div> 
+
      </div>
-  </div>
-    
     </>
   )
 }
